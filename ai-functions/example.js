97ff812888eb5de783e88255c17a7bbbc47ecbcb
Nathan Clevenger <nateclev@gmail.com>
2023-10-04 02:03:37 -0500

    Fixed examples

--- ai-functions/example.js
+++ ai-functions/example.js
@@ -10,3 +10,17 @@ for await (const item of list`synonyms for "fun"`) {
 for await (const item of list`fun things to do in Miami`) {
   console.log(item)
 }
+
+const listBlogPosts = (count, topic) => list`${count} blog post titles about ${topic}`
+const writeBlogPost = title => gpt`write a blog post in markdown starting with "# ${title}"`
+
+async function* writeBlog(count, topic) {
+  for await (const title of listBlogPosts(count, topic)) {
+    const content = await writeBlogPost(title)
+    yield { title, content }
+  }
+}
+
+for await (const post of writeBlog(25, 'future of car sales')) {
+  console.log({ post })
+}
